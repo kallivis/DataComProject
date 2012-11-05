8897d5ae9ce1375e11cdcb8f2ced248bb7be7847
jlwheaton <jlwheaton@hotmail.com>
2012-11-05 18:29:36 -0400

    client and server polish

--- UDPProject/Client.java
+++ UDPProject/Client.java
@@ -1,54 +1,74 @@
+/*
+ * Program Name:    Client.java
+ * Author(s):       Jeremy Wheaton, 100105823
+ *                  Cody McCarthy,  100097829
+ * Version:         2.0 - Nov 2, 2012
+ * Purpose:
+ * Can recieve a file sent from Server.java (using UDP).
+ */
+
 import java.io.*;
 import java.net.*;
+
 public class Client {
-  public static void main(String[] args)
-  {
-//    String filename = args[0];
- //   String message = filename; 
- String   message = "FILE_REQUEST";
-    try {
-
-      File file = new File("local_test.txt");
-      FileOutputStream fos = new FileOutputStream(file);
-
-      InetAddress address = InetAddress.getByName("localhost");
-    //byte[] data = message.getBytes();
-    //byte[] buffer = new byte[64000];
-      byte[] sdata = new byte[512]; 
-      byte[] rData = new byte[512];
-      sdata = message.getBytes();
-
-      DatagramSocket socket = new DatagramSocket();
-      socket.setSoTimeout(5000);
-
-      DatagramPacket packet = new DatagramPacket(sdata, sdata.length,
-          InetAddress.getByName("localhost"), 3031);
-
-      socket.send(packet);
-
-
-      DatagramPacket rpacket = new DatagramPacket(rData, rData.length);
-
-      while (true) {
-        socket.receive(rpacket);
-        System.out.println(rpacket.getData().length);
-        fos.write(rpacket.getData(), 0, rpacket.getLength());
-        if (rpacket.getLength() < 512) {
-            System.out.println("File transferred");
-            break;
+    public static void main(String[] args)
+    {
+        if (args.length <= 0)
+        {
+            System.out.println("No file specified!");
+            return;
         }
-      }
+        String filename = args[0];
+        String message = filename;
+        try
+        {
 
-      System.out.println("File length - " + file.length());
+            File file = new File("local_" + message);
+            FileOutputStream fos = new FileOutputStream(file);
 
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    } catch (UnknownHostException e) {
+            InetAddress address = InetAddress.getByName("localhost");
 
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
+            byte[] sdata = new byte[512]; 
+            byte[] rData = new byte[512];
+            sdata = message.getBytes();
+
+            DatagramSocket socket = new DatagramSocket();
+            socket.setSoTimeout(5000);
+
+            DatagramPacket packet = new DatagramPacket(sdata, sdata.length,
+                    InetAddress.getByName("localhost"), 3031);
+
+            socket.send(packet);
+
+            if (message.equals("EXIT"))
+            {
+                System.out.println("Exit request sent.");
+                return;
+            }
+
+            DatagramPacket rpacket = new DatagramPacket(rData, 
+                rData.length);
+
+            System.out.println("Transfer started.");
+            while (true) {
+                socket.receive(rpacket);
+                fos.write(rpacket.getData(), 0, rpacket.getLength());
+                if (rpacket.getLength() < 512) {
+                    System.out.println("File transferred");
+                    break;
+                }
+            }
+
+            System.out.println("File length - " + file.length());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
-
-  }
 }
--- UDPProject/Server.java
+++ UDPProject/Server.java
@@ -1,83 +1,104 @@
+/*
+ * Program Name:    Server.java
+ * Author(s):       Jeremy Wheaton, 100105823
+ *                  Cody McCarthy,  100097829 
+ * Version:         2.0 - Nov 2, 2012
+ * Purpose:
+ * Can send any requested file (using UDP) from the directory that Server.java is run from
+ * to the directory Client.java is run from.
+ */
+
+
 import java.io.*;
 import java.net.*;
 import java.nio.*;
 
 public class Server {
 
-  public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception
+    {
 
-    byte[] buff = new byte[200]; //was 64...
-    DatagramPacket packet = new DatagramPacket(buff, buff.length);
-    DatagramSocket socket = new DatagramSocket(3031);
+        byte[] buff = new byte[200]; //need??
+        DatagramPacket packet = new DatagramPacket(buff, buff.length);
+        DatagramSocket socket = new DatagramSocket(3031);
 
-    System.out.println("Server started at 3031 ...");
+        System.out.println("Server started at 3031 ...");
 
-    while (true) {
-      socket.receive(packet);
-      new Server.ThreadVideo(socket, packet).run();
+        while (true) {
+            socket.receive(packet);
+            new Server.TransferThread(socket, packet).run();
 
-    }
-  }
-
-  public static class ThreadVideo implements Runnable {
-
-    private DatagramPacket packet;
-    private DatagramSocket socket;
-
-    public ThreadVideo(DatagramSocket socket, DatagramPacket packet) {
-      this.packet = packet;
-      this.socket = socket;
-    }
-
-    public void run() {
-      String cmd = new String(packet.getData(), 0, packet.getLength());
-      if (cmd.equals("FILE_REQUEST")) {
-        System.out.println("FILE REQUESTED");
-        try {
-          read_and_send_video(this.packet.getAddress());
-        } catch (IOException e) {
-          e.printStackTrace();
         }
-      } else {
-        System.out.println("Socket Exiting ....");
-        System.exit(0);
-      }
     }
 
+    public static class TransferThread implements Runnable
+    {
 
-    private void read_and_send_video(InetAddress address)
-      throws IOException {
+        private DatagramPacket packet;
+        private DatagramSocket socket;
 
-      File file = new File("test.txt");
-      FileInputStream fis = new FileInputStream(file);
-      DatagramPacket pack;
-
-      int size = 0;
-      byte[] fileBuffer = new byte[(int) file.length()];
-      byte[] buffer = new byte[512];
-     // ByteBuffer bb = ByteBuffer.allocate(2);
-    //  bb.order(ByteOrder.BIG_ENDIAN);
-
-      while (true) {
-        size = fis.read(buffer);
-        System.out.println("Size = " + size);
-        byte[] sizeBuff = new byte[size]; 
-        sizeBuff = buffer;
-        pack = new DatagramPacket(sizeBuff, size, address,
-            packet.getPort());
-        socket.send(pack);
-        if (size < buffer.length) {
-          break;
+        public TransferThread(DatagramSocket socket, DatagramPacket packet) 
+        {
+            this.packet = packet;
+            this.socket = socket;
         }
-        
-      }
 
-      String cmd = "END_REQUEST";
-      pack = new DatagramPacket(cmd.getBytes(), cmd.getBytes().length,
-          address, packet.getPort());
-      socket.send(pack);
+        public void run()
+        {
+            String rfile = new String(packet.getData(), 0, 
+                packet.getLength());
+            if (rfile.equals("EXIT"))
+            {
+                System.out.println("Server Closing.");
+                System.exit(0);
+            }
+            else
+            {
+                System.out.println("Requested: " + rfile);
+                try
+                {
+                    SendStream(this.packet.getAddress(), rfile);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } 
+            }
+        }
 
+
+        private void SendStream(InetAddress address, String 
+            fileName)
+            throws IOException
+            {
+
+            File file = new File(fileName);
+            FileInputStream fis = new FileInputStream(file);
+            DatagramPacket pack;
+
+            int size = 0;
+            byte[] fileBuffer = new byte[(int) file.length()];
+            byte[] buffer = new byte[512];
+            System.out.println("Transfer started...");
+            while (true)
+            {
+                size = fis.read(buffer);
+                byte[] sizeBuff = new byte[size]; 
+                sizeBuff = buffer;
+                pack = new DatagramPacket(sizeBuff, size, address,
+                        packet.getPort());
+                socket.send(pack);
+                if (size < buffer.length)
+                {
+                    System.out.println("Transfer finished.");
+                    break;
+                }
+
+            }
+
+            String cmd = "END_REQUEST";
+            pack = new DatagramPacket(cmd.getBytes(), cmd.getBytes().length,
+                    address, packet.getPort());
+            socket.send(pack);
+
+        }
     }
-
-  }
 }
