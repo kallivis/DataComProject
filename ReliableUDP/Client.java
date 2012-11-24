a1854b4d089027820e98614b529ddb9c557d836d
Cody McCarthy <cody.mccarthy@colibri-software.com>
2012-11-24 18:32:17 -0400

    fixed indenting

--- ReliableUDP/Client.java
+++ ReliableUDP/Client.java
@@ -12,144 +12,123 @@ import java.net.*;
 
 public class Client {
 
-    //This is the size of the packets being sent and recieved 
-    private final static int PACKET_SIZE = 512;
-    private final static int WINDOW_SIZE = 8;
+  //This is the size of the packets being sent and recieved 
+  private final static int PACKET_SIZE = 512;
+  private final static int WINDOW_SIZE = 8;
 
-    public static void main(String[] args)
+  public static void main(String[] args)
+  {
+    // Checks if no command line args were given by usej
+    if (args.length <= 0)
     {
-        // Checks if no command line args were given by usej
-        if (args.length <= 0)
-        {
-            System.out.println("No file specified!");
-            return;
-        }
-        //Sets the filename equal to the command line argument
-        String filename = args[0];
-        String message = filename;
-
-        try
-        {
-            //Creates an InetAddress using localhost  
-            InetAddress address = InetAddress.getByName("localhost");
-            //The byte buffer used for data the client is sending
-            byte[] sdata = new byte[PACKET_SIZE]; 
-            //The byte buffer used for data the client is receiving
-            byte[] rData = new byte[PACKET_SIZE];
-
-            //Puts "Sync" into the send Data
-            sdata = "SYNC".getBytes();            
-
-            DatagramSocket socket = new DatagramSocket();
-            // Socket timesout after 5 seconds to prevent infinite wait
-            socket.setSoTimeout(5000);
-
-            //Creates the packet and puts in the message 
-            DatagramPacket packet = new DatagramPacket(sdata, sdata.length,
-                    address, 3031);
-
-            //Sends packet with the message to the server 
-            socket.send(packet);
-            
-            //Creates the recieve packet
-            DatagramPacket rpacket = new DatagramPacket(rData, 
-                    rData.length);
-
-            socket.receive(rpacket);
-            //Pulls the string out of the recieved packet
-            String cmd1 = new String(rpacket.getData(), 0, 
-                    rpacket.getLength());
-            //Checks if the server sent SYNACK
-            if (cmd1.equals("SYNACK"))
-            {
-                //Puts the file named into the Send Data
-                sdata = filename.getBytes();
-                //Creates a Packet with the Filename
-                packet = new DatagramPacket(sdata, sdata.length,
-                    address, 3031);
-                socket.send(packet);
-            }
-            else
-                return;
-            //Checks if the message is Exit. If so the client exits as well
-            if (message.equals("EXIT"))
-            {
-                System.out.println("Exit request sent.");
-                return;
-            }
-            //Creates a local file to put the data recieved from the server in.
-            File file = new File("local_" + message);
-            //Opens a FileOutputStream to use to write the data in the above file.
-            FileOutputStream fos = new FileOutputStream(file);
-            //Creates the receiving packet for data comming form the server
-
-            System.out.println("Transfer started.");
-            //The loop to received the packets of requested data.
-            while (true) {
-                //Receives a packet sent from server
-                socket.receive(rpacket);
-                
-                //Puts the String "ACK" into Bytes
-                byte[] cmd = "ACK".getBytes();
-                //Creates and sends the ACK packet
-                packet = new DatagramPacket(cmd, cmd.length, address, 3031);
-                socket.send(packet);
-                byte[] info = new byte[4];
-                byte[] data = new byte[rpacket.getData().length - 4];
-
-                System.arraycopy(rpacket.getData(), 0, info, 0, 
-                        4);
-                System.arraycopy(rpacket.getData(), 4, data, 0, 
-                        rpacket.getData().length - 4);
-                System.out.println("Num"+Client.toInt(info, 0));
-                //Checks if the packet size is 0.
-                //If it is it knows the transfer is complete and client ends.
-                if  (rpacket.getLength() == 0)
-                {
-                    System.out.println("File transferred");
-                    break;
-                }
-                //If the packet has data it writes it into the local file.
-                fos.write(data, 0, data.length);
-                //If this packet is smaller than the agree upon size then it knows
-                //that the transfer is complete and client ends.
-                if (rpacket.getLength() < PACKET_SIZE) {
-                    System.out.println("File transferred");
-                    break;
-                }
-            }
-            System.out.println("File length - " + file.length());
-        } 
-        catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } 
-        catch (UnknownHostException e) {
-            e.printStackTrace();
-        } 
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        
+      System.out.println("No file specified!");
+      return;
     }
-   static byte[] toBytes(int i)
+    //Sets the filename equal to the command line argument
+    String filename = args[0];
+    String message = filename;
+
+    try
+    {
+      //Creates an InetAddress using localhost  
+      InetAddress address = InetAddress.getByName("localhost");
+      //The byte buffer used for data the client is sending
+      byte[] sdata = new byte[PACKET_SIZE]; 
+      //The byte buffer used for data the client is receiving
+      byte[] rData = new byte[PACKET_SIZE];
+
+      //Puts "Sync" into the send Data
+      sdata = "SYNC".getBytes();            
+
+      DatagramSocket socket = new DatagramSocket();
+      // Socket timesout after 5 seconds to prevent infinite wait
+      socket.setSoTimeout(5000);
+
+      //Creates the packet and puts in the message 
+      DatagramPacket packet = new DatagramPacket(sdata, sdata.length,
+          address, 3031);
+
+      //Sends packet with the message to the server 
+      socket.send(packet);
+
+      //Creates the recieve packet
+      DatagramPacket rpacket = new DatagramPacket(rData, 
+          rData.length);
+
+      socket.receive(rpacket);
+      //Pulls the string out of the recieved packet
+      String cmd1 = new String(rpacket.getData(), 0, 
+          rpacket.getLength());
+      //Checks if the server sent SYNACK
+      if (cmd1.equals("SYNACK"))
+      {
+        //Puts the file named into the Send Data
+        sdata = filename.getBytes();
+        //Creates a Packet with the Filename
+        packet = new DatagramPacket(sdata, sdata.length,
+            address, 3031);
+        socket.send(packet);
+      }
+      else
+        return;
+      //Checks if the message is Exit. If so the client exits as well
+      if (message.equals("EXIT"))
+      {
+        System.out.println("Exit request sent.");
+        return;
+      }
+      //Creates a local file to put the data recieved from the server in.
+      File file = new File("local_" + message);
+      //Opens a FileOutputStream to use to write the data in the above file.
+      FileOutputStream fos = new FileOutputStream(file);
+      //Creates the receiving packet for data comming form the server
+
+      System.out.println("Transfer started.");
+      //The loop to received the packets of requested data.
+      while (true) {
+        //Receives a packet sent from server
+        socket.receive(rpacket);
+
+        //Puts the String "ACK" into Bytes
+        byte[] cmd = "ACK".getBytes();
+        //Creates and sends the ACK packet
+        packet = new DatagramPacket(cmd, cmd.length, address, 3031);
+        socket.send(packet);
+        byte[] info = new byte[4];
+        byte[] data = new byte[rpacket.getData().length - 4];
+
+        System.arraycopy(rpacket.getData(), 0, info, 0, 
+            4);
+        System.arraycopy(rpacket.getData(), 4, data, 0, 
+            rpacket.getData().length - 4);
+        //System.out.println("Num"+Client.toInt(info, 0));
+        //Checks if the packet size is 0.
+        //If it is it knows the transfer is complete and client ends.
+        if  (rpacket.getLength() == 0)
         {
-            byte[] result = new byte[4];
-
-            result[0] = (byte) (i >> 24);
-            result[1] = (byte) (i >> 16);
-            result[2] = (byte) (i >> 8);
-            result[3] = (byte) (i /*>> 0*/);
-
-            return result;
+          System.out.println("File transferred");
+          break;
         }
-       static int toInt(byte[] bytes, int offset) {
-            int ret = 0;
-            for (int i=0; i<4 && i+offset<bytes.length; i++) {
-                ret <<= 8;
-                ret |= (int)bytes[i] & 0xFF;
-            }
-            return ret;
-
+        //If the packet has data it writes it into the local file.
+        fos.write(data, 0, data.length);
+        //If this packet is smaller than the agree upon size then it knows
+        //that the transfer is complete and client ends.
+        if (rpacket.getLength() < PACKET_SIZE) {
+          System.out.println("File transferred");
+          break;
         }
- 
+      }
+      System.out.println("File length - " + file.length());
+    } 
+    catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } 
+    catch (UnknownHostException e) {
+      e.printStackTrace();
+    } 
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+
+  }
 }
--- ReliableUDP/Server.java
+++ ReliableUDP/Server.java
@@ -14,224 +14,200 @@ import java.nio.*;
 
 public class Server {
 
-    //This is the size of the packets being sent and recieved 
-    private final static int PACKET_SIZE = 512;
-    private final static int WINDOW_SIZE = 8;
+  //This is the size of the packets being sent and recieved 
+  private final static int PACKET_SIZE = 512;
+  private final static int WINDOW_SIZE = 8;
 
-    public static void main(String[] args) throws Exception
-    {
-        //The buffer for the initial message received from the Client
-        byte[] buff = new byte[PACKET_SIZE];
+  public static void main(String[] args) throws Exception
+  {
+    //The buffer for the initial message received from the Client
+    byte[] buff = new byte[PACKET_SIZE];
 
-        //Creates the Packet to receive the request from the Client
-        DatagramPacket packet = new DatagramPacket(buff, buff.length);
-        //Opens a Datagram Socket that the server is running on
-        //Opens it on port 3031
-        DatagramSocket socket = new DatagramSocket(3031);
+    //Creates the Packet to receive the request from the Client
+    DatagramPacket packet = new DatagramPacket(buff, buff.length);
+    //Opens a Datagram Socket that the server is running on
+    //Opens it on port 3031
+    DatagramSocket socket = new DatagramSocket(3031);
 
 
-        byte[] sdata = new byte[PACKET_SIZE]; 
-        System.out.println("Server started at 3031 ...");
+    byte[] sdata = new byte[PACKET_SIZE]; 
+    System.out.println("Server started at 3031 ...");
 
-        //While server is running, Constantly listens for client requests
-        while (true) {
-            //Receives the request packet from the client
-            socket.receive(packet);
-            //Creates a String from the packet Message
-            String cmd = new String(packet.getData(), 0, 
-                    packet.getLength());
-            //Checks if Client sent the SYNC Command
-            if (cmd.equals("SYNC"))
-            {
-                //Puts the String "SYNACK" into bytes
-                sdata = "SYNACK".getBytes();
-                //Creates a packet for the SYNACK
-                DatagramPacket spacket = new DatagramPacket(sdata, sdata.length,
-                        packet.getAddress(), packet.getPort());
-                socket.send(spacket);
-                //Recieves the Packet with the Filename
-                socket.receive(packet);
-                String filename = new String(packet.getData(), 0, 
-                        packet.getLength());
-                if (filename.equals("EXIT"))
-                {
-                    System.out.println("Server Closing.");
-                    System.exit(0);
-                }
-                System.out.println("Filename: "+ filename);
-
-                //starts a new thread to send the packets of file
-                //requested by the client.
-                new Server.TransferThread(socket, packet).run();
-            }
-
+    //While server is running, Constantly listens for client requests
+    while (true) {
+      //Receives the request packet from the client
+      socket.receive(packet);
+      //Creates a String from the packet Message
+      String cmd = new String(packet.getData(), 0, 
+          packet.getLength());
+      //Checks if Client sent the SYNC Command
+      if (cmd.equals("SYNC"))
+      {
+        //Puts the String "SYNACK" into bytes
+        sdata = "SYNACK".getBytes();
+        //Creates a packet for the SYNACK
+        DatagramPacket spacket = new DatagramPacket(sdata, sdata.length,
+            packet.getAddress(), packet.getPort());
+        socket.send(spacket);
+        //Recieves the Packet with the Filename
+        socket.receive(packet);
+        String filename = new String(packet.getData(), 0, 
+            packet.getLength());
+        if (filename.equals("EXIT"))
+        {
+          System.out.println("Server Closing.");
+          System.exit(0);
         }
+        //starts a new thread to send the packets of file
+        //requested by the client.
+        new Server.TransferThread(socket, packet).run();
+      }
+
     }
-    //Thread class for Transfering Packets
-    public static class TransferThread implements Runnable
+  }
+  //Thread class for Transfering Packets
+  public static class TransferThread implements Runnable
+  {
+    //Sets up a packet and Socket
+    private DatagramPacket packet;
+    private DatagramSocket socket;
+
+    //Constructer for the thread Class
+    //Takes a Socket and Packet as parameters
+    public TransferThread(DatagramSocket socket, DatagramPacket packet) 
     {
-        //Sets up a packet and Socket
-        private DatagramPacket packet;
-        private DatagramSocket socket;
-
-        //Constructer for the thread Class
-        //Takes a Socket and Packet as parameters
-        public TransferThread(DatagramSocket socket, DatagramPacket packet) 
-        {
-            this.packet = packet;
-            this.socket = socket;
-        }
-
-        //The run method of the thread used to check on the request
-        //and if a file is requested starts the sendStream 
-        public void run()
-        {
-            //Uses the received packet data from the client to 
-            //setup a string with the requested filename
-            String filename = new String(packet.getData(), 0, 
-                    packet.getLength());
-            System.out.println("Requested: " + filename);
-            try
-            {
-                //Starts the SendStream for the given filename
-                SendStream(filename, socket);
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            } 
-
-        }
-
-        //Method that takes a filename as it's param
-        //and sends the requested file in packets
-        //to the client that requested it. 
-        private void SendStream(String fileName, DatagramSocket socket) throws IOException
-        {
-            //Opens a file and a makes a file object from the
-            //requested filename for reading.
-            File file = new File(fileName);
-
-            //Creates a fileInputStream for reading in the file into a buffer
-            FileInputStream fis = new FileInputStream(file);
-            //Creates a DatagramPacket packet that will be used to send 
-            //data to the client
-            DatagramPacket pack;
-
-            //Size that will hold the size of the buffer being sent
-            int size = 0;
-            // The remaining size that is left to send of the file
-            int remainingSize = (int) file.length(); 
-            byte[] packInfo = toBytes(1);
-            //The buffer that the file will be read into with a max size
-            //determined by the PACKET_SIZE constant
-            byte[] buffer = new byte[PACKET_SIZE-4];
-            System.out.println("Transfer started...");
-
-            socket.setSoTimeout(20);
-            while (true)
-            {
-                //Reads part of the file into the buffer and sets the size of
-                //the amount read
-                size = fis.read(buffer);
-                //Updates the remaining size by the amount read above
-                remainingSize -= size;
-
-                //Creates a new buffer the size of the amount read
-                //This is in case the size is less than the PACKET_SIZE 
-                //constant, so that the buffer is of exact size of the
-                //data that we are sending
-                byte[] sizeBuff = new byte[size+4];
-
-                //Copies the buffer into this new buffer
-                System.arraycopy(packInfo,0, sizeBuff, 0, packInfo.length);
-                System.arraycopy(buffer,0, sizeBuff, 4, size);
-                // sizeBuff = buffer;
-                //Creates a new packet with the above buffer of data.
-                //Uses the initial client packet to get the client's 
-                //address and port.
-                pack = new DatagramPacket(sizeBuff, size+4, packet.getAddress(), packet.getPort());
-
-                //Sends the above packet to the client
-                socket.send(pack);
-                socket.setSoTimeout(100);
-                //If the size send was less than PACKET_SIZE then the last
-                //packet was sent and Server is done transfering
-                RecACK(socket, pack);
-
-                if (size  + 4 < PACKET_SIZE)
-                {
-                    System.out.println("Transfer finished.");
-                    socket.setSoTimeout(0);
-                    break;
-                }
-                //If data was a multiple of PACKET_SIZE then the last packet
-                //when remaining size is equal to 0.
-                //An empty packet is then sent to the client to signal
-                //that the transfer is complete.
-                if (remainingSize == 0)  
-                {
-                    //Creates the empty packet to send to the client to signal
-                    //that the transfer is complete
-                    pack = new DatagramPacket(new byte[0] , 0, 
-                            packet.getAddress(),packet.getPort());
-                    //Sends the empty packet
-                    socket.send(pack);
-                    System.out.println("Transfer finished.");
-                    socket.setSoTimeout(0);
-                    break;
-                }
-
-
-
-            }
-        }
-        //This method Continually checks to see if the sent pack has been 
-        //ACKed and if not sends it again and calls itself to check again.
-        private void RecACK(DatagramSocket socket, DatagramPacket pack)
-        {
-            byte[] buff = new byte[PACKET_SIZE];
-
-            DatagramPacket packet = new DatagramPacket(buff, buff.length);
-
-            try
-            {
-                socket.receive(packet);
-
-                String cmd = new String(packet.getData(), 0,
-                        packet.getLength());
-                //If client did not ACK send packet again and recall itself
-                if (!cmd.equals("ACK"))
-                {
-                    socket.send(pack);
-                    RecACK(socket, pack);
-                }
-            }
-            catch (IOException e)
-            {
-                //Do Nothing
-            }
-        }
-        
+      this.packet = packet;
+      this.socket = socket;
     }
-    static byte[] toBytes(int i)
+
+    //The run method of the thread used to check on the request
+    //and if a file is requested starts the sendStream 
+    public void run()
+    {
+      //Uses the received packet data from the client to 
+      //setup a string with the requested filename
+      String filename = new String(packet.getData(), 0, 
+          packet.getLength());
+      System.out.println("Requested: " + filename);
+      try
+      {
+        //Starts the SendStream for the given filename
+        SendStream(filename, socket);
+      }
+      catch (IOException e) {
+        e.printStackTrace();
+      } 
+
+    }
+
+    //Method that takes a filename as it's param
+    //and sends the requested file in packets
+    //to the client that requested it. 
+    private void SendStream(String fileName, DatagramSocket socket) throws IOException
+    {
+      //Opens a file and a makes a file object from the
+      //requested filename for reading.
+      File file = new File(fileName);
+
+      //Creates a fileInputStream for reading in the file into a buffer
+      FileInputStream fis = new FileInputStream(file);
+      //Creates a DatagramPacket packet that will be used to send 
+      //data to the client
+      DatagramPacket pack;
+
+      //Size that will hold the size of the buffer being sent
+      int size = 0;
+      // The remaining size that is left to send of the file
+      int remainingSize = (int) file.length(); 
+      byte[] packInfo = ByteConverter.toBytes(1);
+      //The buffer that the file will be read into with a max size
+      //determined by the PACKET_SIZE constant
+      byte[] buffer = new byte[PACKET_SIZE-4];
+      System.out.println("Transfer started...");
+
+      socket.setSoTimeout(20);
+      while (true)
+      {
+        //Reads part of the file into the buffer and sets the size of
+        //the amount read
+        size = fis.read(buffer);
+        //Updates the remaining size by the amount read above
+        remainingSize -= size;
+
+        //Creates a new buffer the size of the amount read
+        //This is in case the size is less than the PACKET_SIZE 
+        //constant, so that the buffer is of exact size of the
+        //data that we are sending
+        byte[] sizeBuff = new byte[size+4];
+
+        //Copies the buffer into this new buffer
+        System.arraycopy(packInfo,0, sizeBuff, 0, packInfo.length);
+        System.arraycopy(buffer,0, sizeBuff, 4, size);
+        // sizeBuff = buffer;
+        //Creates a new packet with the above buffer of data.
+        //Uses the initial client packet to get the client's 
+        //address and port.
+        pack = new DatagramPacket(sizeBuff, size+4, packet.getAddress(), packet.getPort());
+
+        //Sends the above packet to the client
+        socket.send(pack);
+        //If the size send was less than PACKET_SIZE then the last
+        //packet was sent and Server is done transfering
+        RecACK(socket, pack);
+
+        if (size  + 4 < PACKET_SIZE)
         {
-            byte[] result = new byte[4];
-
-            result[0] = (byte) (i >> 24);
-            result[1] = (byte) (i >> 16);
-            result[2] = (byte) (i >> 8);
-            result[3] = (byte) (i /*>> 0*/);
-
-            return result;
+          System.out.println("Transfer finished.");
+          socket.setSoTimeout(0);
+          break;
         }
-        static int toInt(byte[] bytes, int offset) {
-            int ret = 0;
-            for (int i=0; i<4 && i+offset<bytes.length; i++) {
-                ret <<= 8;
-                ret |= (int)bytes[i] & 0xFF;
-            }
-            return ret;
-
+        //If data was a multiple of PACKET_SIZE then the last packet
+        //when remaining size is equal to 0.
+        //An empty packet is then sent to the client to signal
+        //that the transfer is complete.
+        if (remainingSize == 0)  
+        {
+          //Creates the empty packet to send to the client to signal
+          //that the transfer is complete
+          pack = new DatagramPacket(new byte[0] , 0, 
+              packet.getAddress(),packet.getPort());
+          //Sends the empty packet
+          socket.send(pack);
+          System.out.println("Transfer finished.");
+          socket.setSoTimeout(0);
+          break;
         }
 
+
+
+      }
+    }
+    //This method Continually checks to see if the sent pack has been 
+    //ACKed and if not sends it again and calls itself to check again.
+    private void RecACK(DatagramSocket socket, DatagramPacket pack)
+    {
+      byte[] buff = new byte[PACKET_SIZE];
+
+      DatagramPacket packet = new DatagramPacket(buff, buff.length);
+
+      try
+      {
+        socket.receive(packet);
+
+        String cmd = new String(packet.getData(), 0,
+            packet.getLength());
+        //If client did not ACK send packet again and recall itself
+        if (!cmd.equals("ACK"))
+        {
+          socket.send(pack);
+          RecACK(socket, pack);
+        }
+      }
+      catch (IOException e)
+      {
+        //Do Nothing
+      }
+    }
+
+  }
 }
