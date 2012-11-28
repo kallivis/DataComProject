5226cce4d26b7f9d50d7d6e7f599d68f97a55c74
Cody McCarthy <cody.mccarthy@colibri-software.com>
2012-11-27 23:44:28 -0400

    stuff

--- ReliableUDP/ByteConverter.java
+++ ReliableUDP/ByteConverter.java
@@ -12,6 +12,18 @@ public class ByteConverter {
 
     return result;
   }
+  static byte[] toBytes2(int i)
+  {
+    byte[] result = new byte[4];
+
+    result[3] = (byte) (i >> 24);
+    result[2] = (byte) (i >> 16);
+    result[1] = (byte) (i >> 8);
+    result[0] = (byte) (i /*>> 0*/);
+
+    return result;
+  }
+
   static int toInt(byte[] bytes, int offset) {
     int ret = 0;
     for (int i=0; i<4 && i+offset<bytes.length; i++) {
--- ReliableUDP/test.java
+++ ReliableUDP/test.java
@@ -1,13 +1,46 @@
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
+import java.util.BitSet;
+import java.nio.charset.StandardCharsets;
+import java.nio.ByteOrder;
 public class test
 {
   public static void main(String[] args)
   {
-    ByteBuffer bb = ByteBuffer.wrap(ByteConverter.toBytes(4)); 
+    //ByteBuffer bb = ByteBuffer.putInt(4);
+    //BitSet bs = BitSet.valueOf(ByteConverter.toBytes2(4)); 
+    ByteBuffer bb = ByteBuffer.wrap("hello".getBytes(StandardCharsets.US_ASCII)).order(ByteOrder.BIG_ENDIAN);
+    System.out.println(bb.hashCode());
+    //BitSet bs = BitSet.valueOf("hello".getBytes(StandardCharsets.US_ASCII));
+    BitSet bs = BitSet.valueOf(bb);
+    
+    
 
-    IntBuffer ib = bb.asIntBuffer();
-    int int1 = ib.get(0);
-        System.out.println(int1);
+        System.out.println(bs.toString());
+               System.out.println(bs.get(39));
+               System.out.println(bs.get(38));
+        System.out.println(bs.get(37));
+        System.out.println(bs.get(36));
+        System.out.println(bs.get(35));
+        System.out.println(bs.get(34));
+        System.out.println(bs.get(33));
+        System.out.println(bs.get(32));
+        System.out.println(bs.get(31));
+        System.out.println(bs.get(30));
+        System.out.println(bs.get(29));
+        System.out.println(bs.get(28));
+        System.out.println(bs.get(27));
+        System.out.println(bs.get(26));
+        System.out.println(bs.get(25));
+        System.out.println(bs.get(24));
+        System.out.println(bs.get(23));
+        System.out.println(bs.get(22));
+        System.out.println(bs.get(21));
+        System.out.println(bs.get(20));
+
+
+
+
+        System.out.println(bs.size());
   }
 }
